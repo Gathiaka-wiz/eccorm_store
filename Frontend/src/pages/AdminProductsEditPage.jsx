62c7d2be76354b4d5ade0899c5e8aea9d8d4a0bd
John Gathiaka <johngathiakaw@gmail.com>
2025-08-09 22:51:09 +0300

    Refactor Product link format, add Admin pages, and create empty Cart, Checkout, and User pages

---
+++ Frontend/src/pages/AdminProductsEditPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminProductsEditPage = () => {
+    return (
+        <div>AdminProductsEditPage</div>
+    )
+}
+
+export default AdminProductsEditPage
---
+++ Frontend/src/pages/AdminUsersPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AdminUsersPage = () => {
+    return (
+        <div>AdminUsersPage</div>
+    )
+}
