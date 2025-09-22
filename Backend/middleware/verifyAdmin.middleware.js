2f69e2b0a16d647a54d2ca32b73c3a117c99104d
John Gathiaka <johngathiakaw@gmail.com>
2025-09-22 21:01:46 +0300

    Refactor verifyAdmin middleware to prevent infinite load

--- Backend/middleware/verifyAdmin.middleware.js
+++ Backend/middleware/verifyAdmin.middleware.js
@@ -1,23 +1,21 @@
-import User from "../models/user.model.js";
+import User from '../models/user.model.js';
 
 const verifyAdmin = async (req, res, next) => {
-    try {
-        const id = req.userId;
-        
-        const admin = await User.findById( id );
+	try {
+		// const admin = await User.findById(req.userId).select('-password');
+		const admin = await User.findOne({ _id: req.userId });
 
-        if ( admin.role !== "admin" ) {
-            const error = new Error("Method not allowed, Admin only");
-            error.statusCode = 403;
-            throw error;
-        }
-
-        
-    } catch (err) {
-        const error = new Error(`Error in verifyAdmin middleware: ${err}`);
-        error.statusCode = 500;
-        next( error );
-    }
-}
+		if (admin.role !== 'admin') {
+			const error = new Error(`Method not allowed, Admin access only`);
+			error.statusCode = 403;
+			throw error;
+		}
+		next();
+	} catch (err) {
+		const error = new Error(`Error in verifyAdmin middleware: ${err}`);
+		error.statusCode = 500;
+		next(error);
+	}
+};
 
 export default verifyAdmin;
