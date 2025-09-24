8df48f67f05132a1620885b47ff18905de46ba37
John Gathiaka <johngathiakaw@gmail.com>
2025-09-24 12:21:24 +0300

    Update create and update product route to upload image before validating data received

--- Backend/routes/admin.route.js
+++ Backend/routes/admin.route.js
@@ -1,28 +1,51 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 // middleware import
-import verifyToken from "../middleware/verifyToken.middleware.js";
-import verifyAdmin from "../middleware/verifyAdmin.middleware.js";
-import upload from "../middleware/upload.js"
-import { productCreationValidation, productEditValidation, productDeletionValidation } from "../middleware/validator.middleware.js";
+import verifyToken from '../middleware/verifyToken.middleware.js';
+import verifyAdmin from '../middleware/verifyAdmin.middleware.js';
+import upload from '../middleware/upload.js';
+import {
+	productCreationValidation,
+	productEditValidation,
+	productDeletionValidation,
+} from '../middleware/validator.middleware.js';
 
 // controller import
-import { getAllUsers, createProduct, editProduct, deleteProduct, checkAdmin } from "../controllers/admin.controller.js";
+import {
+	getAllUsers,
+	createProduct,
+	editProduct,
+	deleteProduct,
+	checkAdmin,
+} from '../controllers/admin.controller.js';
 
 const router = Router();
 
 router.use(verifyToken);
 router.use(verifyAdmin);
 
-router.get('/check-admin', checkAdmin)
+router.get('/check-admin', checkAdmin);
 
 router.get('/users', getAllUsers);
 
-router.post('/create-product', productCreationValidation, upload.single('image'),  createProduct );
+router.post(
+	'/create-product',
+	upload.single('image'),
+	productCreationValidation,
+	createProduct
+);
 
-router.patch('/:product_id/edit-product', productEditValidation, upload.single('image'),  editProduct );
+router.patch(
+	'/:product_id/edit-product',
+	upload.single('image'),
+	productEditValidation,
+	editProduct
+);
 
-router.delete('/:product_id/delete-product', productDeletionValidation, deleteProduct );
-
+router.delete(
+	'/:product_id/delete-product',
+	productDeletionValidation,
+	deleteProduct
+);
 
 export const AdminRoutes = router;
