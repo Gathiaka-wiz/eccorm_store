d49b5a0f6ce43e9ddd66fe9a801897863067a19a
John Gathiaka <johngathiakaw@gmail.com>
2025-09-29 21:28:10 +0300

    Update error handling

--- Frontend/src/sections/ProductsSection.jsx
+++ Frontend/src/sections/ProductsSection.jsx
@@ -14,17 +14,18 @@ const ProductsSection = () => {
 			id="Products"
 			className="grid grid-cols-2 justify-center  text-center  h-vh w-screen py-10  gap-y-5 bg-gradient-to-br from-orange-400/90 via-orange-300 to-orange-200 bg-no-repeat bg- bg-center max-xs:gap-1 max-xs:py-5 sm:grid-cols-3 min-pc:grid-cols-4   "
 		>
-			{products !== null
+			{products
 				? products.map((prd) => (
-						<Product
-							key={prd._id}
-							id={prd._id}
-							name={prd.product_name}
-							price={prd.price}
-							image={prd.image.url}
-						/>
-				  ))
-				: ''}
+					<Product
+						key={prd._id}
+						id={prd._id}
+						name={prd.product_name}
+						price={prd.price}
+						image={prd.image.url}
+					/>
+				))
+				: null}
+
 		</main>
 	);
 };
